9f207c61a01945d0914fa501a078029f6e49d4fc
Deepak Kumar <deepakkumarnd@gmail.com>
2024-02-02 14:48:10 +0530

    Add tailwindcss and some basic styling

---
+++ config/tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    colors: {
+      primary: "#e7b460", // cadmium yellow
+      secondary: "#54422d", // coffee brown
+      night: "#0a0c0e", // night black
+      space: "#404344", // space gray
+      white: "#ffffff"
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
